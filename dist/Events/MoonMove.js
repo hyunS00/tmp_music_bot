462da72cdfc229137b9a702c24d5e928a6dbef4d
dazzypark <parkjunseo0623@naver.com>
2023-07-21 11:17:53 +0900

    not required file delete

--- dist/Events/MoonMove.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    name: "playerMove",
-    type: "moon",
-    async execute(player, newVoiceChannel, oldVoiceChannel) {
-        console.log(newVoiceChannel, oldVoiceChannel);
-    },
-};
